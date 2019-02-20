575c9d3661026c244c80a241d6b7a0e271174d51
Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>
2019-02-21 08:43:29 +0900

    Update pri launcher
    * Add --base-virtaddr option
    * Output to log
    
    Signed-off-by: Yasufumi Ogawa <ogawa.yasufumi@lab.ntt.co.jp>

--- pri.sh
+++ pri.sh
@@ -6,7 +6,11 @@ set -x
 sudo rm -f $PRI_SOCK11
 sudo rm -f $PRI_SOCK12
 
+# clean /tmp/sock*
+sudo rm -f /tmp/sock*
+
 LOGLEVEL=8
+PRI_LOG=spp_primary.log
 
 sudo $SPP_DIR/src/primary/x86_64-native-linuxapp-gcc/spp_primary \
     -l $PRI_CORE_LIST \
@@ -16,8 +20,10 @@ sudo $SPP_DIR/src/primary/x86_64-native-linuxapp-gcc/spp_primary \
     --proc-type primary \
     --vdev eth_vhost11,iface=$PRI_SOCK11 \
     --vdev eth_vhost12,iface=$PRI_SOCK12 \
+    --base-virtaddr 0x100000000 \
     --log-level $LOGLEVEL \
     -- \
     -p 0x03 \
     -n 10 \
     -s $SPP_HOST_IP:5555
+    > ${SPP_DIR}/log/${PRI_LOG} 2>&1 &
